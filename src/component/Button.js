1d361b9870c9005ad0f997e042f48c11471804ee
Gagansankhla <63205462+Gagansankhla@users.noreply.github.com>
2023-01-11 01:37:56 +0530

    Study about maps, task and fonts

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,9 +1,25 @@
-const Button = ({ color, text }) => {
+import propTypes from 'prop-types'
+
+const Button = ({ color, text, onClick }) => {
   return (
-    <button style={{ backgroundColor: color }} className="btn">
+    <button 
+    onClick={onClick} 
+    style={{ backgroundColor: color }} 
+    className="btn"
+    >
       {text}
     </button>
-  );
-};
+  )
+}
+
+Button.defaultProps = {
+  color : 'steelblue'
+}
+
+Button.propTypes = {
+  text: propTypes.string,
+  color: propTypes.string,
+  onClick: propTypes.func,
+}
 
 export default Button;
--- src/component/Header.js
+++ src/component/Header.js
@@ -2,14 +2,19 @@ import propTypes from "prop-types";
 import Button from "./Button";
 
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Click');
+    }
     return (
+        
         <header className="header">
             <h1>{title}</h1>
-           <Button color = 'green' text= 'Add'/>
+           <Button color = 'green' text= 'Add' onClick=
+           {onClick}/>
            {/* <Button color = 'Blue' text= 'Hello1'/>
            <Button color = 'Pink' text= 'Hello2'/> */}
         </header>
-    );
+    ); 
 };
 
 Header.defaultProps = {
@@ -20,7 +25,7 @@ Header.defaultProps = {
 Header.propTypes = {
     title: propTypes.string.isRequired,
 };
-
+//Css in JS
 // const headingstyle = {
 //     color: 'red', 
 //     backgroundColor: 'black'
